c34d019a5d6a01ae51ac4d75c6834a8fc1e89aea
Amer Toukan <toukan.amer@gmail.com>
2024-11-18 13:31:09 -0700

    Widget sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import * as impact from "@saasquatch/squatch-js";
 
+const tenantAlias = process.env.REACT_APP_TENANT_ALIAS 
+const JWT = process.env.REACT_APP_JWT
 function App() {
+  impact.ready(()=> {
+    window.squatchTenant = tenantAlias
+    window.impactToken = JWT
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <impact-embed widget="p/28196/w/referrerWidget">loading</impact-embed>
     </div>
   );
 }
